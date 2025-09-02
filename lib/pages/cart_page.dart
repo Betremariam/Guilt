66cf1773a50daab756ee90dd9204b49f0959fcc0
Betremariam <yihenewbmariam@gmail.com>
2025-09-02 13:02:31 -0500

    making the cart page to watch and read

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import '../provider/cart_provider.dart';
 
 class CartPage extends StatelessWidget {
@@ -8,43 +7,23 @@ class CartPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final cart = CartProvider();
-    final cartList = cart.cartItems;
+    final cartItems = context.watch<CartProvider>().cartItems;
+
     return Scaffold(
       appBar: AppBar(title: const Text('Cart')),
       body: ListView.builder(
-        itemCount: cartList.length,
+        itemCount: cartItems.length,
         itemBuilder: (context, index) {
-          final carts = cartList[index];
-          return Card(
-            margin: EdgeInsets.symmetric(horizontal: 16, vertical: 20),
-            child: ListTile(
-              leading: Image.asset(
-                carts.imagePath,
-                width: 50,
-                height: 50,
-                fit: BoxFit.cover,
-                errorBuilder:
-                    (context, error, stackTrace) =>
-                        const Icon(Icons.image_not_supported),
-              ),
-              title: Text(carts.name),
-              subtitle: Text('price:${carts.price}'),
-              trailing: Consumer<CartProvider>(
-                builder: (context, cart, child) {
-                  return IconButton(
-                    icon: Icon(Icons.remove),
-                    onPressed: () {
-                      cart.removeItem(cart);
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text('${carts.name} removed succesfully'),
-                        ),
-                      );
-                    },
-                  );
-                },
-              ),
+          final item = cartItems[index];
+          return ListTile(
+            leading: Image.asset(item.imagePath, width: 50, height: 50),
+            title: Text(item.name),
+            subtitle: Text('Price: ${item.price}'),
+            trailing: IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {
+                context.read<CartProvider>().removeItem(item);
+              },
             ),
           );
         },

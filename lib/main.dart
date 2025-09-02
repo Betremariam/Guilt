80876d36293c76cda99491b6657465c4e1c5a6d2
Betremariam <yihenewbmariam@gmail.com>
2025-09-02 12:27:52 -0500

    setting providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
-import '/../pages/home_page.dart';
+import 'package:provider/provider.dart';
+import 'models/fruit_shop.dart';
+import 'models/vegitable_shop.dart';
+import 'pages/home_page.dart';
+import 'provider/cart_provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => CartProvider()),
+        ChangeNotifierProvider(create: (_) => VegitableShop()),
+        ChangeNotifierProvider(create: (_) => FruitShop()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {

349bb459a3e2c80e4f93343749dca3972ddb4dfb
Betremariam <yihenewbmariam@gmail.com>
2025-09-02 12:15:54 -0500

    cart page done

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,1 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../provider/cart_provider.dart';
+
+class CartPage extends StatelessWidget {
+  const CartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final cart = CartProvider();
+    final cartList = cart.cartItems;
+    return Scaffold(
+      appBar: AppBar(title: const Text('Cart')),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final carts = cartList[index];
+          return Card(
+            margin: EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+            child: ListTile(
+              leading: Image.asset(
+                carts.imagePath,
+                width: 50,
+                height: 50,
+                fit: BoxFit.cover,
+                errorBuilder:
+                    (context, error, stackTrace) =>
+                        const Icon(Icons.image_not_supported),
+              ),
+              title: Text(carts.name),
+              subtitle: Text('price:${carts.price}'),
+              trailing: Consumer<CartProvider>(
+                builder: (context, cart, child) {
+                  return IconButton(
+                    icon: Icon(Icons.remove),
+                    onPressed: () {
+                      cart.removeItem(cart);
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('${carts.name} removed succesfully'),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
